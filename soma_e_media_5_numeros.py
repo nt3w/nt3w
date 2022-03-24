3a7dc0ad612c8cb8f5d3d04c2fccdffb1c869d58
wnneto <100881405+wnneto@users.noreply.github.com>
2022-03-24 00:57:20 -0300

    Update soma_e_media_5_numeros.py

--- soma_e_media_5_numeros.py
+++ soma_e_media_5_numeros.py
@@ -9,11 +9,11 @@ import statistics  # Importado a biblioteca para usar a função "mean(média)".
 grupo_num = []  # Lista de números.
 
 while True:
-    grupo_num.append(int(input('Digite o número: ')))
+    grupo_num.append(float(input('Digite o número: ')))
     # Irá ser interrompido até que seja fornecido 5 números.
     if len(grupo_num) == 5:
         # Soma dos 5 números fornecido.
-        print('A soma dos cinco números digitados é: %d' % sum(grupo_num))
+        print('A soma dos cinco números digitados é: %.2f' % sum(grupo_num))
         # Média dos 5 números fornecidos.
         print('Média: %.2f' % statistics.mean(grupo_num))
         break
