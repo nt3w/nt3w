1ed746f2baddcca8bd995c459d28b27b58980975
wnneto <100881405+wnneto@users.noreply.github.com>
2022-03-25 23:08:48 -0300

    Create Metro_cm.py

---
+++ Metro_cm.py
@@ -0,0 +1,7 @@
+# 5. Faça um Programa que converta metros para centímetros
+
+entrada = int(input('Digite os metros: '))
+
+entrada_cm = entrada * 100
+
+print(f'Converdido, fica: {entrada_cm} centímetros')
